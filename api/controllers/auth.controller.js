2ea18ac4685829d1ad90662d218ca8988824ae48
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-01-08 01:28:41 +0400

    complete signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,10 @@
 import User from "../models/user.model.js";
-import bcriptjs from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcriptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
