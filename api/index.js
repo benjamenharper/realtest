0ea1a0fdedb15a62c156a357ebb5d1e230ceadac
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-01-07 17:12:12 +0400

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MongoUrl)
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log("Server is running on port 3000");
 });
