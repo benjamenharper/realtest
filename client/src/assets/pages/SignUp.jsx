6bf2005061745b03e4277996feae2fb76042fe05
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-01-08 23:40:50 +0400

    complete sign in page functionality

--- client/src/assets/pages/SignUp.jsx
+++ client/src/assets/pages/SignUp.jsx
@@ -73,7 +73,7 @@ export default function SignUp() {
         </button>
       </form>
       <div className="flex gap-2 mt-5">
-        <p>have and account?</p>
+        <p>have an account?</p>
         <Link to="/sign-in">
           <span className=" text-blue-700">Sing in</span>
         </Link>
