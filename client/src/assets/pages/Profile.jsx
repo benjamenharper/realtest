bfebd8dba2be5ad5a344a952ffc89b9ba4f0ad68
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-01-10 23:58:47 +0400

    working on image uploading functionalities

--- client/src/assets/pages/Profile.jsx
+++ client/src/assets/pages/Profile.jsx
@@ -1,12 +1,59 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import app from "../components/firebase";
+
+//firebase storage
+// service firebase.storage {
+//   match /b/{bucket}/o {
+//     match /{allPaths=**} {
+//       allow read, write: if
+//       request.resource.size < 2 * 1024 * 1024 && request.resource.contentType.matchs('image/.*');
+//     }
+//   }
+// }
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePercentage, setFilePercentage] = useState(0);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = () => {
+    const storage = getStorage(app);
+    // It should be unique
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed", (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFilePercentage(progress);
+    });
+  };
+
+  console.log(file);
   return (
     <div className=" p-3 max-w-lg mx-auto">
       <h1 className=" text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
         <img
+          onClick={() => fileRef.current.click()}
           className=" rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
           src={currentUser.avatar}
           alt="avatar"
