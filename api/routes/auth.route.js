2a0efe2661810da642f30ba41f457bef2c814617
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-01-13 01:31:42 +0400

    add signOut functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { google, signIn, signUp } from "../controllers/auth.controller.js";
+import {
+  google,
+  signIn,
+  signOut,
+  signUp,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signUp", signUp);
 router.post("/signIn", signIn);
 router.post("/google", google);
+router.get("/signOut", signOut);
 
 export default router;
--- client/src/assets/pages/Profile.jsx
+++ client/src/assets/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure,
 } from "../../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -106,6 +109,23 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signOut");
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -173,7 +193,9 @@ export default function Profile() {
         >
           Delete account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
       <p className=" text-red-700 mt-5">{error ? error : ""}</p>
       <p className=" text-green-700 mt-5">
