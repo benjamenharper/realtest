25265cd52e02de9248bb3ca688ba2a4363dc3f0a
benjamenharper <benjamen.harper@gmail.com>
2025-01-27 14:06:27 -1000

    Add subtle border to header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
   }, [location.search]);
 
   return (
-    <header className="bg-gray-50">
+    <header className="bg-gray-50 border-b border-gray-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-6">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap items-center">
